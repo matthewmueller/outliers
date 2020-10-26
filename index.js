c9a0644eb088b0ae3e6f08cfda417b6799734c62
wholenews <wholenewsorg@gmail.com>
2020-10-26 14:51:36 -0700

    Make g configurable

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function outliers(arr) {
  * @return {Array} outliers
  */
 
-function calc(arr, key) {
+function calc(arr, key, g = 1.5) {
   arr = arr.slice(0);
 
   if (key) arr = arr.map(function(v) { return v[key]; });
@@ -49,7 +49,7 @@ function calc(arr, key) {
 
   var len = arr.length;
   var middle = median(arr);
-  var range = iqr(arr);
+  var range = iqr(arr, g);
   var outliers = [];
 
   for (var i = 0; i < len; i++) {
@@ -82,11 +82,10 @@ function median(arr) {
  * @return {Number}
  */
 
-function iqr(arr) {
+function iqr(arr, g = 1.5) {
   var len = arr.length;
   var q1 = median(arr.slice(0, ~~(len / 2)));
   var q3 = median(arr.slice(Math.ceil(len / 2)));
-  var g = 1.5;
 
   return (q3 - q1) * g;
 }
